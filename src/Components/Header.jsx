6e974e47ed5aeeb9e4b9342567aa551b65df96d1
tejas <tejasjagdale2003@gmail.com>
2022-11-01 20:46:00 +0530

    arews setup with react

---
+++ src/Components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header class="css-16euuc9">
+      <a href="/">
+        <h1 class="css-nedazh">IMAGE FORGERY DETECTION</h1>
+      </a>
+      <div class="css-1h5u173">
+        <div title="Toggle code editor" class="css-548piz">
+          <span class="material-icons css-mknax5">code</span>
+          <span class="css-gwiobj">Code</span>
+        </div>
+        <div title="Toggle preview" class="css-548piz">
+          <span class="material-icons css-mknax5">visibility</span>
+          <span class="css-gwiobj">Preview</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
